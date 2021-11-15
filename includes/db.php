fce7b7bae5a879fd91b61c04fe9ac593dc7153bc
NardosAtnaw <nardidumessa@gmail.com>
2021-11-15 16:31:02 +0300

    db fix

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,13 @@
 <?php
-$connection = mysqli_connect('localhost','root','','design');
+$connection = mysqli_connect('localhost','elegantprintsolu_nardi','d^Nv5Z5lF7$t','elegantprintsolu_design');
+// $connection = mysqli_connect('localhost','root','','fikirlawcom_proclamation');
 
-
+if (!$connection) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
 
 
