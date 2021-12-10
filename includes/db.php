dba120eed71d8eba8c98cf65a07b02d54cbdee1f
NardosAtnaw <nardidumessa@gmail.com>
2021-12-10 13:59:37 +0300

    Maybe

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 // $connection = mysqli_connect('localhost','root','','design');
 $connection = mysqli_connect('localhost','elegantprintsolu_nardi','d^Nv5Z5lF7$t','elegantprintsolu_design');
 
-
+ 
 if (!$connection) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
